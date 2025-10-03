bc47ecafbdd0f45819d97b8eb4742d10881cbd8b
Javiera Valdebenito <javiera.valdebenitoga@gmail.com>
2025-10-03 13:24:30 -0300

    Add files via upload

---
+++ portafolio.js
@@ -0,0 +1,8 @@
+console.log("hola");
+
+const trabajos = [
+        {
+          photo: "endemika.png",
+          h3: "ENDEMIKA",
+          p: "Destacado",
+          description: "Hice este trabajo en este momento, para este curso"
